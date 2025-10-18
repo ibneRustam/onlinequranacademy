0ce719c073c127702096b0901aa6601d754f9181
ibneRustam <abdulqadeerrustam786@gmail.com>
2025-10-18 20:04:11 +0500

    updated code

--- src/app/fee/page.tsx
+++ src/app/fee/page.tsx
@@ -283,10 +283,10 @@ export default function FeesPage() {
           <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 mb-3 sm:mb-4">
             Embark on Your Quran Learning Journey
           </h2>
-          {/* <p className="text-gray-600 text-base sm:text-lg max-w-2xl mx-auto flex items-center justify-center gap-2 flex-col sm:flex-row">
+          <p className="text-gray-600 text-base sm:text-lg max-w-2xl mx-auto flex items-center justify-center gap-2 flex-col sm:flex-row">
            
             Choose a Plan That Fits You - Stay connected every day for less than the cost of your daily coffee
-          </p> */}
+          </p>
           {currentCourse.customClassNote && (
             <p className="text-green-600 font-semibold mt-3 sm:mt-4 text-sm sm:text-base">
               {currentCourse.customClassNote}
@@ -465,9 +465,9 @@ export default function FeesPage() {
                   </span>
                 </div> */}
 
-                <div className="text-center text-xs sm:text-sm text-gray-500 mt-2 sm:mt-4">
+                {/* <div className="text-center text-xs sm:text-sm text-gray-500 mt-2 sm:mt-4">
                   Customize your course - Pay only for what you need
-                </div>
+                </div> */}
               </div>
 
               {/* Features */}
